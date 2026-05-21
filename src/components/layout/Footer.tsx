263efc94aa23a7e2eb71ae5798a7dd6e5ebf11f4
Smart QR Developer <dev@smartqr.com>
2026-05-21 23:37:39 +0530

    Update LinkedIn link in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
     },
     { 
       label: "LinkedIn", 
-      href: "https://linkedin.com/company/yj-developers", 
+      href: "https://www.linkedin.com/in/yuvi-jain-784901410", 
       icon: (props: any) => (
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" {...props}><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
       )
